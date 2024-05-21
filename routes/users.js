c0cd15ef4408acafec9b997061840d11ce6556af
VBcom <115294358+VBcom@users.noreply.github.com>
2024-05-21 03:35:27 +0300

    Create users.js

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+const User = require("../models/User");
+
+router.get("/", async (req, res) => {
+  const users = await User.find();
+
+  res.json(users);
+});
+
+router.post("/", async (req, res) => {
+  const newUser = new User(req.body);
+
+  await newUser.save();
+
+  res.status(201).send(newUser);
+});
+
+module.exports = router;
